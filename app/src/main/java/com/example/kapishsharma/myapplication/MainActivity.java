9ae1c54f0a19646c68fabc8fc10a090796230d41
Kapish630 <Kapishs30@yahoo.com>
2018-12-18 16:42:26 -0500

    added string

--- app/src/main/java/com/example/kapishsharma/myapplication/MainActivity.java
+++ app/src/main/java/com/example/kapishsharma/myapplication/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.kapishsharma.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.logging.ConsoleHandler;
+
 public class MainActivity extends AppCompatActivity {
 
+    String str = "Paul sucks";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
 }
